97104d2e504e674d4fcc2fe98bf65384fec8f673
Thomas Rivet <rivetthom2@gmail.com>
2026-05-09 02:18:11 +0000

    Connect form to Google Apps Script

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Colle ici l'URL de ton Google Apps Script quand il sera déployé.
 // Exemple :
 // window.MULTISOUMISSIONS_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/TON_ID/exec';
-window.MULTISOUMISSIONS_APPS_SCRIPT_URL = '';
+window.MULTISOUMISSIONS_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxO5XY_2lM0pQJ-QPL5r7X0k7a7HDZmapMpgUOxq3NXjfwYyZazcz70zmHsNCL2haSX/exec';
